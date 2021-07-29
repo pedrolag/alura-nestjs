65a7f83b8e0f25e805332ff5dc4cfffa9626e96c
Pedro Goncalves <pedro.goncalves@unimestre.com>
2021-07-29 11:14:21 -0300

    working with product objects instead of strings

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,33 +1,40 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
 
+    products: Product[] = [
+        new Product("BOOK01", "Book 1", 30.90),
+        new Product("BOOK02", "Book 2", 40.90),
+        new Product("BOOK03", "Book 3", 50.90),
+    ]; 
+
     @Get()
-    getAll(): string {
-        return 'List all products';
+    getAll(): Product[] {
+        return this.products;
     }
 
     @Get(':id')
-    get(@Param() params): string {
-        return `Returns the product with id=${params.id}`;
+    get(@Param() params): Product {
+        return this.products[params.id];
     }
 
     @Post()
-    add(@Body() produto): string {
-        console.log(produto);
-        return 'Product added';
+    add(@Body() produto: Product): void {
+        this.products.push(
+            new Product(produto.code, produto.name, produto.price),
+        );
     }
 
     @Put()
-    change(@Body() produto): string {
-        console.log(produto);
-        return 'Product changed';
+    change(@Body() produto: Product): Product {
+        return produto;
     }
 
     @Delete(':id')
-    delete(@Param() params): string {
-        return `Product ${params.id} deleted` 
+    delete(@Param() params): void {
+        this.products.splice(params.id, 1);
     }
 
 }
