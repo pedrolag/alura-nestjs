586480f706b7f82e5e1b1a5a7cb28eba24c7cc7c
Pedro L. A. Gonçalves <pedrolagoncalves@gmail.com>
2021-07-29 23:28:06 -0300

    creating a .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,12 +9,13 @@ import { BooksService } from './books.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'bookstore',
       autoLoadModels: true,
       synchronize: true
